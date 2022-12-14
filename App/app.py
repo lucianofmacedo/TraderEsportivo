6eb5450ce8b1e57812e89b603ceb1b6d31e85f1a
lucianofmacedo <51340504+lucianofmacedo@users.noreply.github.com>
2022-12-14 12:13:37 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -6,7 +6,7 @@ import base64
 st.title("App Análise Luciano Félix Macedo")
 
 st.sidebar.header("Liga")
-selected_league = st.sidebar.selectbox('Liga',['Alemanha 1','Alemanha 2','Bélgica','Escócia 1','Escócia 2','Escócia 3','Escócia 4','Espanha 1','Espanha 2','França 1','França 2','Grécia','Holanda','Inglaterra 1','Inglaterra 2','Inglaterra 3','Inglaterra 4','Inglaterra 5','Italia 1','Italia 2','Portugal','Turquia'])
+selected_league = st.sidebar.selectbox('Liga',['Alemanha - Bundesliga 1','Alemanha - Bundesliga 2','Bélgica - Liga Jupiler','Escócia - Premiere League','Escócia - Divisão 1','Escócia - Divisão 2','Escócia - Divisão 3','Espanha - La Liga 1','Espanha - La Liga 2','França - League 1','França - League 2','Grécia - Super Liga','Holanda - Eredivisie','Inglaterra - Premiere League','Inglaterra - Championship','Inglaterra - League 1','Inglaterra - League 2','Inglaterra - Conference','Italia 1','Itália - Serie A','Itália - Serie B','Portugal - Liga 1','Turquia - Super Liga'])
 
 st.sidebar.header("Temporada")
 selected_season = st.sidebar.selectbox('Temporada', ['2022/2023','2021/2022','2020/2021','2019/2020','2018/2019','2017/2018','2016/2017','2015/2016','2014/2015','2013/2014'])
@@ -14,23 +14,71 @@ selected_season = st.sidebar.selectbox('Temporada', ['2022/2023','2021/2022','20
 # WebScraping Football Data
 def load_data(league, season):
   
-  if selected_league == 'England':
-    league = 'E0'
-  if selected_league == 'Germany':
+  if selected_league == 'Alemanha - Bundesliga 1':
     league = 'D1'
-  if selected_league == 'Italy':
-    league = 'I1'
-  if selected_league == 'Spain':
+  if selected_league == 'Alemanha - Bundesliga 2':
+    league = 'D2'
+  if selected_league == 'Bélgica - Liga Jupiler':
+    league = 'B1'
+  if selected_league == 'Escócia - Premiere League':
+    league = 'SC0'
+  if selected_league == 'Escócia - Divisão 1':
+    league = 'SC1'
+  if selected_league == 'Escócia - Divisão 2':
+    league = 'SC2'
+  if selected_league == 'Escócia - Divisão 3':
+    league = 'SC3'
+  if selected_league == 'Espanha - La Liga 1':
     league = 'SP1'
-  if selected_league == 'France':
+  if selected_league == 'Espanha - La Liga 2':
+    league = 'SP2'
+  if selected_league == 'França - League 1':
     league = 'F1'
-   
+  if selected_league == 'França - League 2':
+    league = 'F2'
+  if selected_league == 'Grécia - Super Liga':
+    league = 'G1'
+  if selected_league == 'Holanda - Eredivisie':
+    league = 'N1'
+  if selected_league == 'Inglaterra - Premiere League':
+    league = 'E0'
+  if selected_league == 'Inglaterra - Championship':
+    league = 'E1'
+  if selected_league == 'Inglaterra - League 1':
+    league = 'E2'
+  if selected_league == 'Inglaterra - League 2':
+    league = 'E3'
+  if selected_league == 'Inglaterra - Conference':
+    league = 'E4'
+  if selected_league == 'Itália - Serie A':
+    league = 'I1'
+  if selected_league == 'Itália - Serie B':
+    league = 'I2'
+  if selected_league == 'Portugal - Liga 1':
+    league = 'P1'
+  if selected_league == 'Turquia - Super Liga':
+    league = 'T1'
+    
+  if selected_season == '2022/2023':
+    season = '2223'
   if selected_season == '2021/2022':
     season = '2122'
   if selected_season == '2020/2021':
     season = '2021'
   if selected_season == '2019/2020':
     season = '1920'
+  if selected_season == '2018/2019':
+    season = '1819'
+  if selected_season == '2017/2018':
+    season = '1718'
+  if selected_season == '2016/2017':
+    season = '1617'
+  if selected_season == '2015/2016':
+    season = '1516'
+  if selected_season == '2014/2015':
+    season = '1415'
+  if selected_season == '2013/2014':
+    season = '1314'
     
   url = "https://www.football-data.co.uk/mmz4281/"+season+"/"+league+".csv"
   data = pd.read_csv(url)
