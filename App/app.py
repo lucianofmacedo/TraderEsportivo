fee27cff5870f3ae502b2a62b0784ccd5588fbc8
lucianofmacedo <51340504+lucianofmacedo@users.noreply.github.com>
2022-12-14 10:41:10 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -1,4 +1,29 @@
+#Importar as bibliotecas
+
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+#Criar título para o App
+
+st.title("Aplicativo Dados Esportivos Luciano Félix Macedo")
+
+#Criação da barra lateral
+
+st.sidebar.header("Liga")
+selected_league = st.sidebar.selectbox('Liga',['Alemanha','Bélgica','Escócia','Espanha','França','Grécia','Holanda','Inglaterra','Italia','Portugal','Turquia']
+
+st.sidebar.header("Temporada")
+selected_league = st.sidebar.selectbox('Temporada',['2023/2022','2022/2021','2021/2020','2020/2019','2019/2018','2018/2017','2017/2016','2016/2015','2015/2014','2014/2013']
+                                       
+#Fazer webscraping Footbal Data
+
+
+  
+def load_data(liga, temporada):
+  url = "https://www.football-data.co.uk/mmz4281/"+Temporada+"/"+Liga+".csv"
+  data = pd.read_csv(url)
+  return data
+
+df - load_data(selected_liga, selected_temporada)
 
