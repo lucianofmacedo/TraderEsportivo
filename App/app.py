b9536bd5632e7287dffd53828dfff5c231a3cd9a
lucianofmacedo <51340504+lucianofmacedo@users.noreply.github.com>
2022-12-14 10:56:21 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -11,17 +11,17 @@ st.title("Aplicativo Dados Esportivos Luciano Félix Macedo")
 #Criação da barra lateral
 
 st.sidebar.header("Liga")
-selected_Liga = st.sidebar.selectbox('Liga',['Alemanha','Bélgica','Escócia','Espanha','França','Grécia','Holanda','Inglaterra','Italia','Portugal','Turquia']
+selected_liga = st.sidebar.selectbox('Liga',['Alemanha','Bélgica','Escócia','Espanha','França','Grécia','Holanda','Inglaterra','Italia','Portugal','Turquia']
 
 st.sidebar.header("Temporada")
-selected_Temporada = st.sidebar.selectbox('Temporada',['2023/2022','2022/2021','2021/2020','2020/2019','2019/2018','2018/2017','2017/2016','2016/2015','2015/2014','2014/2013']
+selected_temporada = st.sidebar.selectbox('Temporada',['2023/2022','2022/2021','2021/2020','2020/2019','2019/2018','2018/2017','2017/2016','2016/2015','2015/2014','2014/2013']
                                        
 #Fazer webscraping Footbal Data
 
 
   
-def load_data(Liga, Temporada):
-  url = "https://www.football-data.co.uk/mmz4281/"+Temporada+"/"+Liga+".csv"
+def load_data(liga, temporada):
+  url = "https://www.football-data.co.uk/mmz4281/"+temporada+"/"+liga+".csv"
   data = pd.read_csv(url)
   return data
 
